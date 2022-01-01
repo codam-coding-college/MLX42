225ec83ddb42f93d606b3aa1019300246d811f27
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-01-02 00:19:16 +0100

    Fix issue with not getting y coordinate.

--- src/mlx_get_mouse_pos.c
+++ src/mlx_get_mouse_pos.c
@@ -6,7 +6,7 @@
 /*   By: W2Wizard <w2.wizzard@gmail.com>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/01 23:20:13 by W2Wizard      #+#    #+#                 */
-/*   Updated: 2022/01/01 23:36:44 by W2Wizard      ########   odam.nl         */
+/*   Updated: 2022/01/02 00:17:34 by W2Wizard      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	mlx_get_mouse_pos(t_MLX *mlx, t_IVec2 *pos_out)
 {
 	double	pos[2];
 
-	glfwGetCursorPos(mlx->window, &pos[0], &pos[0]);
+	glfwGetCursorPos(mlx->window, &pos[0], &pos[1]);
 	pos_out->x = (int32_t)pos[0];
 	pos_out->y = (int32_t)pos[1];
 }
