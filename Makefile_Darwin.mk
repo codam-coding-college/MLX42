2f8f23d3f4d674d4bc01d200c862f03e412a481d
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-01-16 00:41:57 +0100

    Add `-Werror`

--- Makefile_Darwin.mk
+++ Makefile_Darwin.mk
@@ -6,12 +6,12 @@
 #    By: W2Wizard <w2.wizzard@gmail.com>              +#+                      #
 #                                                    +#+                       #
 #    Created: 2021/12/28 01:01:14 by W2Wizard      #+#    #+#                  #
-#    Updated: 2022/01/15 20:47:43 by w2wizard      ########   odam.nl          #
+#    Updated: 2022/01/16 00:41:04 by w2wizard      ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
 NAME 	= mlx42.a
-CFLAGS	= -Wextra -Wall -Wunreachable-code -O3 -g
+CFLAGS	= -Wextra -Wall -Werror -Wunreachable-code -O3 -g
 HEADERS = -I include
 
 # //= Colors =// #
--- Makefile_Linux.mk
+++ Makefile_Linux.mk
@@ -6,11 +6,11 @@
 #    By: W2Wizard <w2.wizzard@gmail.com>              +#+                      #
 #                                                    +#+                       #
 #    Created: 2021/12/28 01:01:14 by W2Wizard      #+#    #+#                  #
-#    Updated: 2022/01/15 20:47:21 by w2wizard      ########   odam.nl          #
+#    Updated: 2022/01/16 00:41:08 by w2wizard      ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
 NAME 	=	mlx42.a
-CFLAGS	=	-Wextra -Wall -Wunreachable-code -O3
+CFLAGS	=	-Wextra -Wall -Werror -Wunreachable-code -O3
 ARCHIVE	=	-ldl -lglfw -lGL -lX11 -lpthread -lXrandr -lXi
 HEADERS =	-I include
