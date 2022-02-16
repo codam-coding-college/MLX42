88d858c123dc45a3cc4a40f34a7d7afb7df4098c
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-02-17 00:04:32 +0100

    Add png decoding function

--- src/textures/mlx_png.c
+++ src/textures/mlx_png.c
@@ -6,15 +6,35 @@
 /*   By: W2Wizard <w2.wizzard@gmail.com>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/16 23:11:29 by W2Wizard      #+#    #+#                 */
-/*   Updated: 2022/02/16 23:21:30 by W2Wizard      ########   odam.nl         */
+/*   Updated: 2022/02/17 00:03:55 by W2Wizard      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MLX42/MLX42_Int.h"
 
-t_mlx_texture	*mlx_load_png(const char *path)
+/**
+ * Decode/load a PNG file onto a buffer.
+ * 
+ * NOTE: BPP will always be 4 due using decode32.
+ * 
+ * @param path Path to the PNG file.
+ * @return If successful the texture data is returned, else NULL.
+ */
+t_mlx_texture	*mlx_decode_png(const char *path)
 {
-	(void) path;
+	uint32_t		error;
+	t_mlx_texture	*image;
 
-	return (NULL);
+	image = malloc(sizeof(t_mlx_texture));
+	if (!image)
+		return ((void *)mlx_log(MLX_ERROR, MLX_MEMORY_FAIL));
+	error = lodepng_decode32_file(&(image->pixels), \
+	((uint32_t *)&image->width), ((uint32_t *)&image->height), path);
+	if (error)
+	{
+		free(image);
+		return ((void *)mlx_log(MLX_ERROR, lodepng_error_text(error)));
+	}
+	image->bytes_per_pixel = sizeof(int32_t);
+	return (image);
 }
