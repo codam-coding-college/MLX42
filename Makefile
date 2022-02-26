108b53a4d9967e229566dbf7369bfd8f6e3db92c
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-02-26 22:14:21 +0100

    Remove unused archives & corrections

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: W2Wizard <w2.wizzard@gmail.com>              +#+                      #
 #                                                    +#+                       #
 #    Created: 2022/02/26 21:32:49 by W2Wizard      #+#    #+#                  #
-#    Updated: 2022/02/26 21:32:49 by W2Wizard      ########   odam.nl          #
+#    Updated: 2022/02/26 22:13:15 by W2Wizard      ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,17 +20,17 @@ ifdef DEBUG
 CFLAGS	+= -g
 endif
 
-#TODO: Add extra define for cygwin for github actions and potential other people
 ifeq ($(OS), Windows_NT)
-	ARCHIVE	= -lglfw3 -lopengl32 -lgdi32
-    include Makefile_Windows.mk
+	ifdef CYGWIN
+		include Makefile_Unix.mk
+	else
+    	include Makefile_WindowsNT.mk
+	endif
 else
     UNAME_S := $(shell uname -s)
     ifeq ($(UNAME_S), Linux)
-		ARCHIVE	= -ldl -lglfw -lGL -lX11 -lpthread -lXrandr -lXi
         include Makefile_Unix.mk
 	else ifeq ($(UNAME_S), Darwin)
-		ARCHIVE	= -lglfw
 		DYLIB_EXISTS = test -e /usr/local/lib/libglfw.3.dylib || echo "false"
 		ifneq ($(DYLIB_EXISTS), false)
 			DYLIB_EXISTS = test -e /Users/$(USER)/.brew/opt/glfw/lib/libglfw.3.dylib || echo "false"
@@ -44,4 +44,5 @@ else
     endif
 endif
 
+#//= Misc =//#
 .PHONY: all, clean, fclean, re
--- Makefile_Unix.mk
+++ Makefile_Unix.mk
@@ -1,24 +1,16 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         ::::::::             #
-#    Makefile_unix.mk                                   :+:    :+:             #
+#    Makefile_Unix.mk                                   :+:    :+:             #
 #                                                      +:+                     #
 #    By: W2Wizard <w2.wizzard@gmail.com>              +#+                      #
 #                                                    +#+                       #
 #    Created: 2022/02/26 21:36:38 by W2Wizard      #+#    #+#                  #
-#    Updated: 2022/02/26 21:36:38 by W2Wizard      ########   odam.nl          #
+#    Updated: 2022/02/26 22:12:31 by W2Wizard      ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
-# //= Files =// #
-# /usr/bin/find is explicitly mentioned here for Windows compilation under Cygwin
-SHDR	=	src/mlx_vert.c src/mlx_frag.c
-SHDRSRC	=	shaders/default.frag shaders/default.vert
-LIBS	=	$(shell /usr/bin/find ./lib -iname "*.c")
-SRCS	=	$(shell /usr/bin/find ./src -iname "*.c") $(SHDR) $(LIBS)
-OBJS	=	${SRCS:.c=.o}
-
-# //= Colors =// #
+#//= Colors =//#
 BOLD	= \033[1m
 BLACK	= \033[30;1m
 RED		= \033[31;1m
@@ -30,8 +22,15 @@ CYAN	= \033[36;1m
 WHITE	= \033[37;1m
 RESET	= \033[0m
 
-# //= Rules =// #
-## //= Compile =// #
+#//= Files =//#
+# /usr/bin/find is explicitly mentioned here for Windows compilation under Cygwin
+SHDR	=	src/mlx_vert.c src/mlx_frag.c
+SHDRSRC	=	shaders/default.frag shaders/default.vert
+LIBS	=	$(shell /usr/bin/find ./lib -iname "*.c")
+SRCS	=	$(shell /usr/bin/find ./src -iname "*.c") $(SHDR) $(LIBS)
+OBJS	=	${SRCS:.c=.o}
+
+#//= Recipes =//#
 all: $(SHDR) $(NAME)
 
 # Convert our shaders to .c files
@@ -44,13 +43,11 @@ src/mlx_frag.c: shaders/default.frag
 	@python3 tools/compile_shader.py $^ > $@
 
 %.o: %.c
-	@$(CC) $(CFLAGS) -o $@ -c $< $(HEADERS) $(ARCHIVE) && printf "$(GREEN)$(BOLD)\rCompiling: $(notdir $<)\r\e[35C[OK]\n$(RESET)"
+	@$(CC) $(CFLAGS) -o $@ -c $< $(HEADERS) && printf "$(GREEN)$(BOLD)\rCompiling: $(notdir $<)\r\e[35C[OK]\n$(RESET)"
 
 $(NAME): $(OBJS)
 	@ar rc $(NAME) $(OBJS)
 	@printf "$(GREEN)$(BOLD)Done\n$(RESET)"
-
-## //= Commands =// #
 
 clean:
 	@echo "$(RED)Cleaning$(RESET)"
@@ -60,6 +57,3 @@ fclean: clean
 	@rm -f $(NAME)
 
 re:	fclean all
-
-## //= Misc =// #
-.PHONY: all, clean, fclean, re
