cfe09dd065725c33ab1af74b14d45314e864d830
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-03-01 23:56:39 +0100

    Norme png, texture and xpm

--- src/textures/mlx_png.c
+++ src/textures/mlx_png.c
@@ -6,28 +6,28 @@
 /*   By: W2Wizard <w2.wizzard@gmail.com>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/16 23:11:29 by W2Wizard      #+#    #+#                 */
-/*   Updated: 2022/02/25 15:17:12 by lde-la-h      ########   odam.nl         */
+/*   Updated: 2022/03/01 23:05:02 by lde-la-h      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MLX42/MLX42_Int.h"
 
-t_mlx_texture	*mlx_load_png(const char *path)
+mlx_texture_t* mlx_load_png(const char* path)
 {
-	uint32_t		error;
-	t_mlx_texture	*image;
+	uint32_t error;
+	mlx_texture_t* image;
 
-	image = malloc(sizeof(t_mlx_texture));
-	if (!image)
-		return ((void *)mlx_error(MLX_MEMFAIL));
-	error = lodepng_decode32_file(&image->pixels, &image->width, \
-	&image->height, path);
-	if (error)
+	if (!(image = malloc(sizeof(t_mlx_texture));)))
+		return ((void*)mlx_error(MLX_MEMFAIL));
+
+	image->bytes_per_pixel = sizeof(int32_t);
+	if ((error = lodepng_decode32_file(&image->pixels, &image->width, &image->height, path)))
 	{
 		free(image);
+
+		// Explicitly print error on purpose
 		fprintf(stderr, "MLX42: LodePNG: %s\n", lodepng_error_text(error));
 		return ((void *)mlx_error(MLX_INVPNG));
 	}
-	image->bytes_per_pixel = sizeof(int32_t);
 	return (image);
 }
--- src/textures/mlx_texture.c
+++ src/textures/mlx_texture.c
@@ -6,30 +6,27 @@
 /*   By: W2Wizard <w2.wizzard@gmail.com>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/17 01:02:24 by W2Wizard      #+#    #+#                 */
-/*   Updated: 2022/02/23 13:14:14 by lde-la-h      ########   odam.nl         */
+/*   Updated: 2022/03/01 23:08:45 by lde-la-h      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MLX42/MLX42_Int.h"
 
-t_mlx_image	*mlx_texture_area_to_image(t_mlx *mlx, t_mlx_texture *texture, \
-int32_t xy[2], uint32_t wh[2])
+mlx_image_t* mlx_texture_area_to_image(mlx_t* mlx, mlx_texture_t* texture, int32_t xy[2], uint32_t wh[2])
 {
-	uint32_t	y;
-	uint32_t	bpp;
-	uint8_t		*pixelx;
-	uint8_t		*pixeli;
-	t_mlx_image	*image;
+	uint32_t y = 0;
+	uint8_t* pixelx;
+	uint8_t* pixeli;
+	mlx_image_t* image;
 
-	y = 0;
 	if (!mlx || !texture || !xy || !wh)
-		return ((void *)mlx_error(MLX_NULLARG));
-	bpp = texture->bytes_per_pixel;
+		return ((void*)mlx_error(MLX_NULLARG));
 	if (xy[0] > (int32_t)texture->width || xy[1] > (int32_t)texture->height)
-		return ((void *)mlx_error(MLX_INVAREA));
-	image = mlx_new_image(mlx, wh[0], wh[1]);
-	if (!image)
-		return ((void *)mlx_error(MLX_MEMFAIL));
+		return ((void*)mlx_error(MLX_INVAREA));
+	if (!(image = mlx_new_image(mlx, wh[0], wh[1])))
+		return ((void*)mlx_error(MLX_MEMFAIL));
+
+	uint32_t bpp = texture->bytes_per_pixel;
 	while (y < wh[1])
 	{
 		pixelx = &texture->pixels[((xy[1] + y) * texture->width + xy[0]) * bpp];
@@ -40,43 +37,38 @@ int32_t xy[2], uint32_t wh[2])
 	return (image);
 }
 
-t_mlx_image	*mlx_texture_to_image(t_mlx *mlx, t_mlx_texture *texture)
+mlx_image_t* mlx_texture_to_image(mlx_t* mlx, mlx_texture_t* texture)
 {
-	t_mlx_image		*img;
-	const int32_t	xy[] = {0, 0};
-	const uint32_t	wh[] = {texture->width, texture->height};
+	mlx_image_t* img;
+	const int32_t xy[] = {0, 0};
+	const uint32_t wh[] = {texture->width, texture->height};
 
 	if (!mlx || !texture)
-		return ((void *)mlx_error(MLX_NULLARG));
-	img = mlx_texture_area_to_image(mlx, texture, (int32_t *)xy, \
-	(uint32_t *)wh);
-	if (!img)
-		return ((void *)mlx_error(MLX_MEMFAIL));
+		return ((void*)mlx_error(MLX_NULLARG));
+	if (!(img = mlx_texture_area_to_image(mlx, texture, (int32_t *)xy, (uint32_t *)wh)))
+		return ((void*)mlx_error(MLX_MEMFAIL));
 	return (img);
 }
 
-bool	mlx_draw_texture(t_mlx_image *image, t_mlx_texture *texture, int32_t x, \
-int32_t y)
+bool mlx_draw_texture(mlx_image_t* image, mlx_texture_t* texture, int32_t x, int32_t y)
 {
-	uint32_t	i;
-	uint32_t	j;
-	uint8_t		*pixel;
+	uint32_t j = 0;
+	uint32_t i = 0;
+	uint8_t* pixel;
 
 	if (!texture || !image)
 		return (mlx_error(MLX_NULLARG));
-	if (texture->width > image->width || \
-		texture->height > image->height)
+	if (texture->width > image->width || texture->height > image->height)
 		return (mlx_error(MLX_TEXTOBIG));
-	i = 0;
+
+	// TODO: Remove second while loop, use memmove/cpy, less messy.
 	while (i < texture->height)
 	{
 		j = 0;
-		while (++j < texture->width)
+		while (j < texture->width)
 		{
-			pixel = &texture->pixels[(i * texture->width + j) * \
-			texture->bytes_per_pixel];
-			mlx_putpixel(image, x + j, y + i, *pixel << 24 | \
-			*(pixel + 1) << 16 | *(pixel + 2) << 8 | *(pixel + 3));
+			pixel = &texture->pixels[(i * texture->width + j) * texture->bytes_per_pixel];
+			mlx_putpixel(image, x + j, y + i, *pixel << 24 | *(pixel + 1) << 16 | *(pixel + 2) << 8 | *(pixel + 3));
 			j++;
 		}
 		i++;
@@ -84,12 +76,12 @@ int32_t y)
 	return (true);
 }
 
-void	mlx_delete_texture(t_mlx_texture *texture)
+void mlx_delete_texture(mlx_texture_t* texture)
 {
 	if (!texture)
 	{
 		mlx_error(MLX_NULLARG);
-		return ;
+		return;
 	}
 	mlx_freen(2, texture->pixels, texture);
 }
