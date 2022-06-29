481a763c558c08cc98e03d1548b958455aee0744
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-06-29 16:05:00 +0200

    Fix assertion for put pixel

--- src/mlx_put_pixel.c
+++ src/mlx_put_pixel.c
@@ -6,7 +6,7 @@
 /*   By: W2Wizard <w2.wizzard@gmail.com>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/28 03:30:13 by W2Wizard      #+#    #+#                 */
-/*   Updated: 2022/06/27 20:03:01 by lde-la-h      ########   odam.nl         */
+/*   Updated: 2022/06/29 16:00:30 by lde-la-h      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ void mlx_draw_pixel(uint8_t* pixel, uint32_t color)
 void mlx_put_pixel(mlx_image_t* image, uint32_t x, uint32_t y, uint32_t color)
 {
 	MLX_NONNULL(image);
-	MLX_ASSERT(x > image->width, "Pixel is out of bounds");
-	MLX_ASSERT(y > image->height, "Pixel is out of bounds");
+	MLX_ASSERT(x < image->width, "Pixel is out of bounds");
+	MLX_ASSERT(y < image->height, "Pixel is out of bounds");
 
 	uint8_t* pixelstart = &image->pixels[(y * image->width + x) * BPP];
 	mlx_draw_pixel(pixelstart, color);
