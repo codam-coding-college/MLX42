ff06a8f1e531e972152346636b51d588fff63c9c
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-07-20 11:24:21 +0200

    Improve the output for tester

--- test/Init.c
+++ test/Init.c
@@ -6,7 +6,7 @@
 /*   By: lde-la-h <lde-la-h@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/18 10:19:40 by lde-la-h      #+#    #+#                 */
-/*   Updated: 2022/07/20 10:13:13 by lde-la-h      ########   odam.nl         */
+/*   Updated: 2022/07/20 11:08:51 by lde-la-h      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 int main(void)
 {
 	TEST_DECLARE("test");
-	TEST_EXPECT(FAIL);
+	TEST_EXPECT(PASS);
+
+	assert(false);
 
 	TEST_EXIT(EXIT_SUCCESS);
 }
--- test/Tester.h
+++ test/Tester.h
@@ -6,13 +6,14 @@
 /*   By: lde-la-h <lde-la-h@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/18 12:41:32 by lde-la-h      #+#    #+#                 */
-/*   Updated: 2022/07/20 10:05:28 by lde-la-h      ########   odam.nl         */
+/*   Updated: 2022/07/20 11:15:20 by lde-la-h      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TESTER_H
 #define TESTER_H
 # include <stdio.h>
+# include <string.h>
 # include <stdlib.h>
 # include <fcntl.h>
 # include <errno.h>
@@ -37,7 +38,12 @@ static int32_t _result = -1;
 static bool _normalExit;
 static const char* _name;
 
-int32_t resolveExitCode(void)
+static void printError(void)
+{
+	printf("\r\033[35C\033[31;1m[ERROR] %s\033[0m\n", strerror(errno));
+}
+
+static int32_t resolveExitCode(void)
 {
 	return (_result == _expected ? EXIT_SUCCESS : EXIT_FAILURE);
 }
@@ -46,28 +52,29 @@ void handle(void)
 {
 	static char buff[256] = {0};
 
-	// Something went wrong
+	// Check result
 	if (_result != _expected)
 	{
-		// Signal
-		if (!_normalExit)
-		{
-			ssize_t bread;
-			if ((bread = read(_fds[READ], buff, sizeof(buff))) < 0)
-			{
-				perror("read");
-				return;
-			}
-		}
-		else // Exit code did not match
+		if (_normalExit)
 			_result = FAIL;
 	}
 	else
 		_result = PASS;
 	
+	// If we failed and exit was abnormal
+	if (_result == FAIL && !_normalExit)
+	{
+		ssize_t bread;
+		if ((bread = read(_fds[READ], buff, sizeof(buff))) < 0)
+		{
+			printError();
+			return;
+		}
+	}
+
 	// Print result
-	printf("%s\n", _result == PASS ? "[OK]" : "[FAIL]");
-	printf("%s", buff);
+	printf("\r\033[35C%s\n", _result == PASS ? "\033[32;1m[OK]\033[0m" : "\033[31;1m[FAIL]\033[0m");
+	printf("\033[31;1m%s\033[0m", buff);
 }
 
 #define TEST_EXPECT(expect)		\
@@ -86,22 +93,21 @@ void sighandle(int32_t sig)
 }
 
 // Initialize the function as a test.
-#define TEST_DECLARE(Name)				\
-	_name = Name;						\
-	printf("Testing: %s -> ", _name);	\
-	if (pipe(_fds) < 0) { 				\
-		perror("pipe");					\
-		exit(EXIT_FAILURE);				\
-	}									\
-	dup2(_fds[WRITE], STDERR_FILENO);	\
-	/* Now redirect the signals!	*/	\
-	atexit(handle);						\
-	signal(SIGABRT, sighandle);			\
-	signal(SIGSEGV, sighandle);			\
-	signal(SIGILL,	sighandle);			\
-	signal(SIGTERM, sighandle);			\
-	signal(SIGKILL, sighandle);			\
-	signal(SIGBUS,	sighandle);			\
+#define TEST_DECLARE(Name)							\
+	_name = Name;									\
+	printf("\033[30;1mTesting: %s\033[0m", _name);	\
+	if (pipe(_fds) < 0) { 							\
+		printError();								\
+		exit(EXIT_FAILURE);							\
+	}												\
+	dup2(_fds[WRITE], STDERR_FILENO);				\
+	/* Now redirect the signals!	*/				\
+	atexit(handle);									\
+	signal(SIGABRT, sighandle);						\
+	signal(SIGSEGV, sighandle);						\
+	signal(SIGILL,	sighandle);						\
+	signal(SIGTERM, sighandle);						\
+	signal(SIGBUS,	sighandle);						\
 
 #endif
 
