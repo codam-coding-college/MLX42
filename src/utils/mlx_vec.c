5e782e8db0ab43f90e1cbf9a60fc410e9a067bc5
W2Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-08-21 14:04:35 +0200

    Implement vector clear
    Co-authored-by: VNC <VNNCC@users.noreply.github.com>

--- src/utils/mlx_vec.c
+++ src/utils/mlx_vec.c
@@ -14,11 +14,11 @@
 
 //= Private =//
 
-static void mlx_vector_resize(mlx_vec_t* v, int capacity)
+static void mlx_vector_resize(mlx_vec_t* v, int32_t capacity)
 {
 	MLX_NONNULL(v);
 
-	void **data = realloc(v->data, sizeof(void*) * capacity);
+	void* data = realloc(v->data, v->elementSize * capacity);
 	if (data) 
 	{
 		v->data = data;
@@ -51,7 +51,7 @@ void mlx_vector_push_back(mlx_vec_t* v, void* item)
     v->count++;
 }
 
-void mlx_vector_set(mlx_vec_t* v, size_t index, void* item)
+void mlx_vector_set(mlx_vec_t* v, int32_t index, void* item)
 {
 	MLX_NONNULL(v);
 	MLX_ASSERT(index >= 0 && index < v->count, "Out of bounds");
@@ -59,7 +59,7 @@ void mlx_vector_set(mlx_vec_t* v, size_t index, void* item)
 	memcpy(v->data + (index * v->elementSize), item, v->elementSize);
 }
 
-void* mlx_vector_get(mlx_vec_t* v, size_t index)
+void* mlx_vector_get(mlx_vec_t* v, int32_t index)
 {
 	MLX_NONNULL(v);
 	MLX_ASSERT(index >= 0 && index < v->count, "Out of bounds");
@@ -67,7 +67,23 @@ void* mlx_vector_get(mlx_vec_t* v, size_t index)
 	return (v->data + (index * v->elementSize)); 
 }
 
-void mlx_vector_delete(mlx_vec_t* v, size_t index)
+void mlx_vector_swap(mlx_vec_t* v, int32_t srcIndex, int32_t dstIndex)
+{
+	MLX_NONNULL(v);
+	MLX_ASSERT(srcIndex >= 0 && srcIndex < v->count, "Src Out of bounds");
+	MLX_ASSERT(dstIndex >= 0 && dstIndex < v->count, "Dst Out of bounds");
+
+	int32_t srcPosition = v->elementSize * srcIndex;
+	int32_t dstPosition = v->elementSize * dstIndex;
+
+	void* temp = alloca(v->elementSize);
+
+	memcpy(temp, v->data + srcPosition, v->elementSize);
+	memcpy(v->data + srcPosition, v->data + dstPosition, v->elementSize);
+	memcpy(v->data + dstPosition, temp, v->elementSize);
+}
+
+void mlx_vector_delete(mlx_vec_t* v, int32_t index)
 {
 	MLX_NONNULL(v);
 	MLX_ASSERT(index >= 0 && index < v->count, "Out of bounds");
@@ -96,7 +112,15 @@ void mlx_vector_delete(mlx_vec_t* v, size_t index)
 		mlx_vector_resize(v, v->capacity / 2);
 }
 
+void mlx_vector_clear(mlx_vec_t* v)
+{
+	v->position = 0;
+	v->count = 0;
+}
+
 void mlx_vector_free(mlx_vec_t* v)
 {
+	mlx_vector_clear(v);
 	free(v->data);
+	v->capacity = 0;
 }
