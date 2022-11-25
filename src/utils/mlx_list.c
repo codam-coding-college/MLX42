95e8b7818b7cec6486ee8a53abc75e872094464e
Jeroen Van Halderen <jvan-hal@f0r5s5.codam.nl>
2022-11-25 15:13:32 +0100

    remove the copying of arguments

--- src/utils/mlx_list.c
+++ src/utils/mlx_list.c
@@ -6,7 +6,7 @@
 /*   By: W2Wizard <w2.wizzard@gmail.com>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/28 01:53:51 by W2Wizard      #+#    #+#                 */
-/*   Updated: 2022/11/25 15:10:25 by jvan-hal      ########   odam.nl         */
+/*   Updated: 2022/11/25 15:12:53 by jvan-hal      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,9 +100,6 @@ bool mlx_equal_image(void* lstcontent, void* value)
 
 bool mlx_equal_inst(void* lstcontent, void* value)
 {
-	const draw_queue_t* lcontent = lstcontent;
-	const mlx_image_t* lvalue = value;
-
 	return ((draw_queue_t *)lstcontent->image == (mlx_image_t *)value);
 }
 
