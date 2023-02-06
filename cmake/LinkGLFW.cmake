9cbc2262f2454cd4f3b5f606c83d9d8cbc7651ca
W2.Wizard <main@w2wizard.dev>
2023-02-06 22:04:43 +0100

    Bump GLFW version

--- cmake/LinkGLFW.cmake
+++ cmake/LinkGLFW.cmake
@@ -4,7 +4,7 @@ macro(LinkGLFW TARGET)
     FetchContent_Declare(
         glfw
         GIT_REPOSITORY https://github.com/glfw/glfw
-        GIT_TAG 3.3.2
+        GIT_TAG 3.3.8
     )
 
     FetchContent_GetProperties(glfw)
